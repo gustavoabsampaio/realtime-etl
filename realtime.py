4330954cc3ca941adaa59f9bac9add8afbd4fbdd
Gustavo A B Sampaio <gustavoabsampaio@gmail.com>
2023-07-17 05:57:21 -0300

    adjustments to transform task, load task done, main flow running

--- realtime.py
+++ realtime.py
@@ -1,18 +1,23 @@
 import requests
 import pandas as pd
-from prefect import flow, task
-from datetime import datetime
-from pytz import timezone
 
+from datetime import datetime
+from prefect import flow, task
+from pytz import timezone
+from sqlalchemy import create_engine
+
+@task
 def extract():
     print("EXTRACT START")
 
+    # request urls
     base_url = "https://api.data.gov.sg/v1/environment/"
 
     airtemp_url = base_url + 'air-temperature'
 
     rainfall_url = base_url + 'rainfall'
 
+    # request datetime and date
     raw_datetime = datetime.now(timezone('Asia/Singapore'))
     date_time = raw_datetime.strftime('%Y-%m-%dT%H:%M:%S')
     date = raw_datetime.strftime('%Y-%m-%d')
@@ -28,25 +33,18 @@ def extract():
         "Content-Type": "application/json"
     }
 
-    # Send the GET request
+    # Send the GET requests
     try:
         response_data_airtemp = requests.get(airtemp_url, params=params, headers=headers)
-        print(f"Response status: {response_data_airtemp.status_code}")
-        print(f"Response content: {response_data_airtemp.content}")
     except requests.exceptions.RequestException as e:
-        print(f"ERROR on the airtemp GET request: {e}")
         response_data_airtemp = None
-        raise ValueError("ERROR on the airtemp GET request ", e)
+        raise ValueError("ERROR on the airtemp GET request: ", e)
 
     try:
         response_data_rainfall = requests.get(rainfall_url, params=params, headers=headers)
-        print(f"Response status: {response_data_rainfall.status_code}")
-        print(f"Response content: {response_data_rainfall.content}")
     except requests.exceptions.RequestException as e:
-        # print(f"ERROR on the rainfall GET request: {e}")
         response_data_rainfall = None
-        raise ValueError("ERROR on the rainfall GET request ", e)
-        
+        raise ValueError("ERROR on the rainfall GET request: ", e)
 
     print("EXTRACT END")
 
@@ -54,11 +52,16 @@ def extract():
 
     return response_data
 
-def transform(response_data):
+@task
+def transform(response_data):    
+    print("DATA FILTERING START")
+
+    if error_sim % 10 == 0:
+        raise ValueError("TEST ERROR")
+
     if response_data == None:
-        print("Empty data")
-        return
-    print("Data Filtering Start")
+        raise ValueError("No data")
+
     response_data_airtemp = response_data[0].json()
     response_data_rainfall = response_data[1].json()
     
@@ -77,26 +80,63 @@ def transform(response_data):
 
     readings_airtemp['timestamp'] = timestamp_df
 
+    # Merge stations and readings dataframes
     df = pd.merge(stations, readings_airtemp, left_on='id', right_on='station_id')
     df = pd.merge(df, readings_rainfall, left_on='id', right_on='station_id', suffixes=(None, "_y"))
+
+    # Drop unnecessary columns and rename columns
     df = df.drop(['id','station_id_y'], axis=1)
-    df = df.rename(columns={'station_id': 'station_id', 'value_x': 'airtemp', 'value_y': 'rainfall'})
-    # print(stations)
-    print(df)
-    print("Data Filtering End\n")
+    df = df.rename(columns={'station_id': 'station_id', 'value': 'temperature', 'value_y': 'rainfall', 'timestamp': 'measurement_timestamp', 'name': 'station_name'})
+
+    df['latitude'] = df['location'].apply(lambda x: x['latitude'])
+    df['longitude'] = df['location'].apply(lambda x: x['longitude'])
+
+    # Drop the original 'location' column
+    df.drop('location', axis=1, inplace=True)
+
+    # print(df)
+
+    print("DATA FILTERING END\n")
     return df
 
-def load():
-    pass
+@task
+def load(transformed_data):
+    try:
+        print("LOAD START")
+
+        # connect to the database
+        engine = create_engine('postgresql://gustavo:postgres@localhost:5432/gustavo')
+
+        table = 'weather_data'
+
+        # convert Dataframe to psql and append to table
+        transformed_data.to_sql(table, engine, if_exists='append', index=False, method='multi')
+
+        print("LOAD END\n")
+
+    except Exception as e:
+        print("Data insertion failed: ", e)
 
 def print_readings(filtered_df):
     # Print the filtered readings
     for _, row in filtered_df.iterrows():
         print(f"Station: {row['station_id']}, Name: {row['name']}, Lat/Long: ({row['location']['latitude']}, {row['location']['longitude']}), Temperature: {row['value']}°C, Horario(SGT): {row['timestamp']}")
 
+@flow
 def main_flow():
     response_data = extract()
-    response_df = transform(response_data)
-    print_readings(response_df)
+    transformed_data = transform(response_data)
+    load(transformed_data)
+    # print_readings(transformed_data)
     
-main_flow()
+
+# every 10 runs there will be an exception to test the exception handling and pipeline consistency
+error_sim = 1
+
+while True:
+    # main flow
+    # return state must be true to keep the pipeline running in case of exceptions
+    main_flow(return_state=True)
+    error_sim += 1
+
+# main_flow()
