d2c18b930fd1e921c4a8e9c1ab5d067248526c5d
Gustavo A B Sampaio <gustavoabsampaio@gmail.com>
2023-07-18 07:18:00 -0300

    select statements sql file added

---
+++ select_statements.sql
@@ -0,0 +1,11 @@
+-- Media de temperatura por dia
+SELECT * FROM average_temperatures_daily;
+
+-- Media de temperatura nas ultimas 24h
+SELECT * FROM average_temperatures_24h;
+
+-- Quantidade de chuva total por dia
+SELECT * FROM total_rain_daily;
+
+-- Quantidade de chuva total nas ultimas 4h
+SELECT * FROM total_rain_4h;
